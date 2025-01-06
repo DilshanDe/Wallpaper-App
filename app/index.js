55eca726797fd39515ba10da6d0c348a1b747aa9
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2025-01-06 11:17:09 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
 const WelcomeScreen = () => {
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
+    <View style={styles.container}>
+      <StatusBar style='light'/>
+      <Image 
+      source={require('../assets/images/welcome.png')}
+      style={styles.bgImage}
+      resizeMode='cover'
+      />
     </View>
   )
 }
+
+const styles=StyleSheet.create({
+
+})
 
 export default WelcomeScreen
